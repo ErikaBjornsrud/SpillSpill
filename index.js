c17114d60fd9bcc3b44d00fc2d4d7a22f00ef65a
ErikaBjornsrud <erikabjo@viken.no>
2024-05-16 12:01:19 +0200

    yey fikk til js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const vid = document.getElementById("video")
 
-const snake_hover = document.getElementById("snake_hover")
-const react_hover = document.getElementById("react_hover")
+// Konstanter 
+const react_hover = document.getElementById("react_hover");
+const snake_hover = document.getElementById("snake_hover");
+
+// When hover, change color to black 
+snake_hover.addEventListener("mouseover", function() {
+    hover(snake_hover);
+});
+
+react_hover.addEventListener("mouseover", function() {
+    hover(react_hover);
+});
+
+function hover(hoverElm) {
+    hoverElm.style.color = "black";
+    console.log("hei");
+}
 
 
-const tv = document.getElementById("TV")
+// Brings back the white color 
+snake_hover.addEventListener("mouseout", function() {
+    out(snake_hover);
+});
 
+react_hover.addEventListener("mouseout", function() {
+    out(react_hover);
+});
 
-tv.addEventListener("click", function () {
-    react_hover.style.color = "black"
-    tv.style.backgroundColor = "blue"
-})
+function out(outElm) {
+    outElm.style.color = "white";
+}
+
 
